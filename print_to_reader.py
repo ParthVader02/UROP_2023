2d01f1885930a708b223d4fa6f713fcdc7285818
Parth <pp537@cam.ac.uk>
2023-07-28 14:05:13 +0100

    updated print_to_reader.py

--- print_to_reader.py
+++ print_to_reader.py
@@ -1,5 +1,13 @@
 import time
 import ctypes
+import socket
+import numpy as np
+
+def random_word(letters): # generate a random word of length 3
+	word = ""
+	for i in range(0, 3):
+		word += letters[np.random.randint(0, len(letters) - 1)]
+	return word
 
 #Load the NVDA client library
 clientLib=ctypes.windll.LoadLibrary('./nvdaControllerClient64.dll')
@@ -10,9 +18,11 @@ if res!=0:
 	errorMessage=str(ctypes.WinError(res))
 	ctypes.windll.user32.MessageBoxW(0,u"Error: %s"%errorMessage,u"Error communicating with NVDA",0)
 
-#Speak and braille some messages
-words= ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
-for word in words:
-	clientLib.nvdaController_brailleMessage(word)
-	time.sleep(1)
-#clientLib.nvdaController_brailleMessage(u"Test completed!")
+#braille some messages
+change_flag = False
+letters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"]
+word = random_word(letters)
+while True:
+	if change_flag == False:
+		clientLib.nvdaController_brailleMessage(word)
+	
