fbf9442fd9e4969571938432047a7f00362966b4
Parth <pp537@cam.ac.uk>
2023-08-08 14:12:13 +0100

    performance for 5000 data

--- Generic_ur5_controller/colour_grad.py
+++ Generic_ur5_controller/colour_grad.py
@@ -4,9 +4,8 @@ import os
 img = cv2.imread('raw_data/im5.jpg')
 b,g,r = cv2.split(img)
 
-grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow("grey", grey)
 cv2.imshow("b", b)
 cv2.imshow("g", g)
 cv2.imshow("r", r)
+
 cv2.waitKey(0)
