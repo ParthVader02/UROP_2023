24edd35f47a2b37c470cdaf122e69322e06caaab
Parth <pp537@cam.ac.uk>
2023-08-30 12:12:13 +0100

    text file processing

--- Generic_ur5_controller/text_file_process.py
+++ Generic_ur5_controller/text_file_process.py
@@ -1,28 +1,30 @@
-with open("originaltext.txt","r") as temp_file:
+import csv
+with open("testing.txt","r") as temp_file:
     file = temp_file.read()
-
 print(file)
 
-# the next 6 lines are different in order to have a more accurate word count.
-# entirely unnecessary if you're not fussed by word count
-spacetext = ""
+spacetext = "" #initialise string of text with spaces
 for i in file: # loops through every character in the text
     if i.isalpha() or i==" " or i=="\n": # removes any punctuation, numbers
         spacetext += i.lower() # adds the lowercase version of any letters
-plaintext = spacetext.replace(" ","")
-plaintext = plaintext.replace("\n","")
-
+plaintext = spacetext.replace(" ","") # removes spaces
+plaintext = plaintext.replace("\n","") # removes new lines
 print(plaintext)
 
-splitplain = [plaintext[i:i+20] for i in range(0, len(plaintext), 20)]
-for i in range(len(splitplain)): splitplain[i] += "\n"
+splitplain = [plaintext[i:i+20] for i in range(0, len(plaintext), 20)] # split into lines of 20 characters
+for i in range(len(splitplain)): splitplain[i] += "\n" # add new line character to end of each line
 
-plaintext_file = open("plaintext.txt","w")
-plaintext_file.writelines(splitplain)
-plaintext_file.close()
+plaintext_file = open("new_testing.txt","w") # write to new text file -> this file is put into braille reader SD card
+plaintext_file.writelines(splitplain) # write each line
+plaintext_file.close() # close file
 
 wordno = len(spacetext.split()) # number of words
 lettno = len(plaintext) # number of letters
-lineno = len(splitplain) # number of lines [== int(np.ceil(len(plaintext)/20))]
+lineno = len(splitplain) # number of lines, same as doing int(np.ceil(len(plaintext)/20))
 
+props = [str(wordno),str(lettno),str(lineno)] # list of properties
+
+with open('test_properties.csv', 'w') as f: #append string of letters, velocity and times to csv
+    write = csv.writer(f)
+    write.writerow(props) #write proerties to csv
 print(wordno,lettno,lineno)
